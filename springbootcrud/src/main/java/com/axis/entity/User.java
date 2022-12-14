0eb517d3a6afa3134deeeab74106d4ac73bdbf30
Shri1912 <88429145+Shri1912@users.noreply.github.com>
2022-12-14 22:02:48 +0530

    Add files via upload

---
+++ springbootcrud/src/main/java/com/axis/entity/User.java
@@ -0,0 +1,64 @@
+package com.axis.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastname;
+	
+	@Column(name="email")
+	private String email;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public User() {
+		
+	}
+	
+	public User(String firstName, String lastname, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastname = lastname;
+		this.email = email;
+	}
+	
+	
+
+}
